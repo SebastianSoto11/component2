1a4dcb7c7c265b2458cde1e6a1a7c0b71f5b704e
SebastianSoto11 <sebast.soto@newhorizons.edu.do>
2021-12-02 01:04:50 +0000

    commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BotonComponent } from './boton/boton.component';
+import { Boton2Component } from './boton2/boton2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BotonComponent,
+    Boton2Component
   ],
   imports: [
     BrowserModule,
